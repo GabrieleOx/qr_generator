841a6d797b91dceb91cc8fc59bdeb636ce65228b
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2025-06-23 11:14:22 +0200

    Update to version 1.1 (PcVersion): Added color chooser for qrcode Removed file extension selection (useless)

--- PcVersion(Java)/src/main/java/com/generator/App.java
+++ PcVersion(Java)/src/main/java/com/generator/App.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.nio.file.Paths;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,6 +20,7 @@ import javax.swing.filechooser.FileSystemView;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageConfig;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 
@@ -28,13 +29,17 @@ public final class App {
     }
 
     public static void main(String[] args) throws WriterException, IOException {
+        Color [] colors = new Color[2];
         JFrame windowFrame = new JFrame("QR Generator");
-        JLabel pathLabel = new JLabel("Dove?"), linkLabel = new JLabel("Link:"), fileNameLabel = new JLabel("Nome del file:"), extensionLabel = new JLabel("Estensione del file:");
+        JLabel pathLabel = new JLabel("Dove?"), linkLabel = new JLabel("Link:"), fileNameLabel = new JLabel("Nome del file:"), colorsLabel = new JLabel("Colori qr code:");
         JTextField linkTextField = new JTextField("www.esempio.com"), fileNameTextField = new JTextField("ExampleQrCode"), pathTextField = new JTextField("_Path_");
         JFileChooser pathChooser = new JFileChooser("/", FileSystemView.getFileSystemView());
-        JCheckBox jpgCheckBox = new JCheckBox(".JPG"), pngCheckBox = new JCheckBox(".PNG");
-        JButton sendButton = new JButton("Crea"), pathButton = new JButton("Seleziona");
-        JPanel linkPanel = new JPanel(), pathPanel = new JPanel(), fileNamePanel = new JPanel(), extensionPanel = new JPanel(), checkBoxPanel = new JPanel(), selectionPanel = new JPanel();
+        JColorChooser colorChooser = new JColorChooser();
+        JButton sendButton = new JButton("Crea"), pathButton = new JButton("Seleziona"), qrcodeColorButton = new JButton("Colore del pattern"), backgroundColorButton = new JButton("Colore dello sfondo");
+        JPanel linkPanel = new JPanel(), pathPanel = new JPanel(), fileNamePanel = new JPanel(), colorsPanel = new JPanel(), colorChooserPanel = new JPanel(), selectionPanel = new JPanel();
+
+        colors[0] = Color.black;
+        colors[1] = null;
 
         //Graphical part
 
@@ -42,7 +47,7 @@ public final class App {
         windowFrame.setResizable(false);
         windowFrame.getContentPane().setBackground(new Color(214, 144, 30));
         windowFrame.setLayout(null);
-        windowFrame.add(linkPanel); windowFrame.add(pathPanel); windowFrame.add(fileNamePanel); windowFrame.add(extensionPanel); windowFrame.add(sendButton);
+        windowFrame.add(linkPanel); windowFrame.add(pathPanel); windowFrame.add(fileNamePanel); windowFrame.add(colorsPanel); windowFrame.add(sendButton);
 
         linkPanel.setBounds(20, 20, 200, 100);
         linkPanel.setLayout(new GridLayout(0, 1));
@@ -74,19 +79,18 @@ public final class App {
         fileNameTextField.setBackground(new Color(241, 245, 203));
         fileNamePanel.add(fileNameLabel); fileNamePanel.add(fileNameTextField);
 
-        extensionPanel.setBounds(330, 200, 250, 100);
-        extensionPanel.setLayout(new GridLayout(0, 1));
-        extensionPanel.setBackground(null);
-        checkBoxPanel.setLayout(new GridLayout(1, 0));
-        checkBoxPanel.setBackground(null);
-        jpgCheckBox.setFont(new Font("Verdana", Font.PLAIN, 16));
-        jpgCheckBox.setBackground(null);
-        pngCheckBox.setFont(new Font("Verdana", Font.PLAIN, 16));
-        pngCheckBox.setBackground(null);
-        pngCheckBox.setSelected(true);
-        checkBoxPanel.add(jpgCheckBox); checkBoxPanel.add(pngCheckBox);
-        extensionLabel.setFont(new Font("Verdana", Font.BOLD, 18));
-        extensionPanel.add(extensionLabel); extensionPanel.add(checkBoxPanel);
+        colorsPanel.setBounds(300, 200, 250, 100);
+        colorsPanel.setLayout(new GridLayout(0, 1));
+        colorsPanel.setBackground(null);
+        colorChooserPanel.setLayout(new GridLayout(0, 1));
+        colorChooserPanel.setBackground(null);
+        qrcodeColorButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        qrcodeColorButton.setBackground(new Color(241, 245, 203));
+        backgroundColorButton.setFont(new Font("Verdana", Font.BOLD, 16));
+        backgroundColorButton.setBackground(new Color(241, 245, 203));
+        colorChooserPanel.add(qrcodeColorButton);colorChooserPanel.add(backgroundColorButton);
+        colorsLabel.setFont(new Font("Verdana", Font.BOLD, 18));
+        colorsPanel.add(colorsLabel); colorsPanel.add(colorChooserPanel);
 
         sendButton.setBounds(200, 360, 150, 50);
         sendButton.setFont(new Font("Verdana", Font.BOLD, 20));
@@ -100,37 +104,59 @@ public final class App {
             pathTextField.setText(pathChooser.getSelectedFile().getPath());
         });
 
-        jpgCheckBox.addActionListener(ActionListener -> {
-            pngCheckBox.setSelected(!pngCheckBox.isSelected());
-        });
-
-        pngCheckBox.addActionListener(ActionListener -> {
-            jpgCheckBox.setSelected(!jpgCheckBox.isSelected());
-        });
-
         sendButton.addActionListener(ActionListener -> {
-            String path, link = linkTextField.getText(), name = fileNameTextField.getText(), ext;
-            BitMatrix mat;
+            String path, link = linkTextField.getText(), name = fileNameTextField.getText();
 
-            if(jpgCheckBox.isSelected())
-                ext = "jpg";
-            else if(pngCheckBox.isSelected())
-                ext = "png";
-            else ext = "jpeg"; // as a default (you never know...)
-
-            path = pathTextField.getText() + '\\' + name + '.' + ext;
+            path = pathTextField.getText() + '\\' + name + ".png";
 
             try {
-                mat = new MultiFormatWriter().encode(link, BarcodeFormat.QR_CODE, 400, 400);
-                MatrixToImageWriter.writeToPath(mat, ext, Paths.get(path));
-                JOptionPane.showMessageDialog(windowFrame,"Qr code creato correttamente","Alert",JOptionPane.WARNING_MESSAGE);
-            } catch (WriterException | IOException e1) {
-                JOptionPane.showMessageDialog(windowFrame,"Errore nella creazione:\n" + e1.getMessage(),"Alert",JOptionPane.WARNING_MESSAGE);
+                generateQr(path, link, windowFrame, colors);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(windowFrame,"Errore nella creazione:\n" + e.getMessage(),"Alert",JOptionPane.WARNING_MESSAGE);
             }
+            
             windowFrame.dispose();
+        });
+
+        qrcodeColorButton.addActionListener(ActionListener -> {
+            colors[0] = JColorChooser.showDialog(windowFrame, "Colore Pattern", colors[0]);
+        });
+
+        backgroundColorButton.addActionListener(ActionListener -> {
+            colors[1] = JColorChooser.showDialog(windowFrame, "Colore Sfondo", Color.white);
         });
 
         windowFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         windowFrame.setVisible(true);
+    }
+
+    private static void generateQr(String path, String link, JFrame window, Color [] colori) throws IOException{
+        BitMatrix mat;
+        int on = rgbToInt(colori[0].getRed(), colori[0].getBlue(), colori[0].getGreen(), colori[0].getAlpha()), off;
+
+        if(colori[1] != null)
+            off = rgbToInt(colori[1].getRed(), colori[1].getBlue(), colori[1].getGreen(), colori[1].getAlpha());
+        else off = 0;
+
+        MatrixToImageConfig config = new MatrixToImageConfig(on, off);
+        
+        try {
+            mat = new MultiFormatWriter().encode(link, BarcodeFormat.QR_CODE, 400, 400);
+            MatrixToImageWriter.writeToPath(mat, "png", Paths.get(path), config);
+            JOptionPane.showMessageDialog(window,"Qr code creato correttamente","Alert",JOptionPane.WARNING_MESSAGE);
+        } catch (WriterException e1) {
+            JOptionPane.showMessageDialog(window,"Errore nella creazione:\n" + e1.getMessage(),"Alert",JOptionPane.WARNING_MESSAGE);
+        }
+    }
+
+    private static int clamp(int val, int min, int max){
+        return Math.max(min, Math.min(max, val));
+    }
+    private static int rgbToInt(int red, int blue, int green, int alpha){
+        alpha = clamp(alpha, 0, 255);
+        red = clamp(red, 0, 255);
+        blue = clamp(blue, 0, 255);
+        green = clamp(green, 0, 255);
+        return (alpha << 24) | (red << 16) | (green << 8) | blue;
     }
 }
